f3851a6776ce14832efa2158b43a4b54ddff5af4
Michael 'Mickey' Lauer <mickey@vanille-media.de>
2009-05-23 00:49:19 +0200

    use standard autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="vala-dbus-binding-tool"
+ORIGDIR=`pwd`
+cd $srcdir
 
-. gnome-autogen.sh
+autoreconf -v --install || exit 1
+cd $ORIGDIR || exit $?
+
+$srcdir/configure "$@"
